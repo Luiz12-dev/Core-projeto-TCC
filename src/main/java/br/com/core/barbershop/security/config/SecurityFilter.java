96e1b494bb79071737bedab19893d5560225da5c
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2025-12-03 13:58:43 -0300

    feat(Security): Adiciona Anotação @Component

--- src/main/java/br/com/core/barbershop/security/config/SecurityFilter.java
+++ src/main/java/br/com/core/barbershop/security/config/SecurityFilter.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import br.com.core.barbershop.security.jwt.TokenService;
@@ -16,6 +17,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class SecurityFilter extends OncePerRequestFilter{
 
     private static final Logger logger = LoggerFactory.getLogger(SecurityFilter.class);
