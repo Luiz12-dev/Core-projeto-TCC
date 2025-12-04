673ada5fba2330a28e4fda27700f6586c02faec8
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2025-12-04 14:12:43 -0300

    feat(repository): Cria lógica para consulta no banco de dados

--- src/main/java/br/com/core/barbershop/repository/BarberServiceRepository.java
+++ src/main/java/br/com/core/barbershop/repository/BarberServiceRepository.java
@@ -1,5 +1,6 @@
 package br.com.core.barbershop.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +8,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.core.barbershop.domain.BarberService;
 
 public interface BarberServiceRepository extends JpaRepository<BarberService, UUID>{
-    
+
+    Optional<BarberService> findByServiceName(String serviceName);
+    Optional<BarberService> deledeleteByServiceName(String serviceName);
+    Boolean exiexistsByServiceName(String serviceName);
 
 }
